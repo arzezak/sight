a64239e266d65cbdaab310aa665497442f1a7771
Ariel Rzezak <arzezak@gmail.com>
2026-03-07 09:54:29 -0300

    DRY up annotation formatter test helper
    Add optional lines: param to make_annotation so all tests
    use the shared helper instead of building hunks manually.
    Also alphabetize requires in lib/sight.rb.

--- lib/sight.rb
+++ lib/sight.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "sight/version"
-require_relative "sight/display_line"
-require_relative "sight/diff_parser"
-require_relative "sight/git"
 require_relative "sight/annotation"
 require_relative "sight/annotation_formatter"
+require_relative "sight/app"
 require_relative "sight/claude_hook_installer"
 require_relative "sight/cli"
-require_relative "sight/app"
+require_relative "sight/diff_parser"
+require_relative "sight/display_line"
+require_relative "sight/git"
+require_relative "sight/version"
 
 module Sight
   class Error < StandardError; end
--- test/test_annotation_formatter.rb
+++ test/test_annotation_formatter.rb
@@ -8,8 +8,9 @@ class TestAnnotationFormatter < Minitest::Test
   end
 
   def test_hunk_annotation
-    ann = make_annotation("foo.rb", "looks good")
-    out = Sight::AnnotationFormatter.format([ann])
+    out = Sight::AnnotationFormatter.format([
+      make_annotation("foo.rb", "looks good")
+    ])
 
     assert_includes out, "## File: foo.rb"
     assert_includes out, "```diff"
@@ -17,13 +18,13 @@ class TestAnnotationFormatter < Minitest::Test
   end
 
   def test_only_changed_lines
-    hunk = Sight::Hunk.new(context: "def foo", lines: [
-      Sight::DiffLine.new(type: :ctx, content: " unchanged", lineno: 1),
-      Sight::DiffLine.new(type: :del, content: "-old", lineno: nil),
-      Sight::DiffLine.new(type: :add, content: "+new", lineno: 2)
+    out = Sight::AnnotationFormatter.format([
+      make_annotation("bar.rb", "ok", lines: [
+        Sight::DiffLine.new(type: :ctx, content: " unchanged", lineno: 1),
+        Sight::DiffLine.new(type: :del, content: "-old", lineno: nil),
+        Sight::DiffLine.new(type: :add, content: "+new", lineno: 2)
+      ])
     ])
-    ann = Sight::Annotation.new(file_path: "bar.rb", type: :hunk, hunk: hunk, comment: "ok")
-    out = Sight::AnnotationFormatter.format([ann])
 
     assert_includes out, "-old"
     assert_includes out, "+new"
@@ -31,9 +32,10 @@ class TestAnnotationFormatter < Minitest::Test
   end
 
   def test_multiple_annotations_same_file
-    a1 = make_annotation("foo.rb", "first")
-    a2 = make_annotation("foo.rb", "second")
-    out = Sight::AnnotationFormatter.format([a1, a2])
+    out = Sight::AnnotationFormatter.format([
+      make_annotation("foo.rb", "first"),
+      make_annotation("foo.rb", "second")
+    ])
 
     assert_equal 1, out.scan("## File: foo.rb").size
     assert_includes out, "> first"
@@ -41,9 +43,10 @@ class TestAnnotationFormatter < Minitest::Test
   end
 
   def test_multiple_files
-    a1 = make_annotation("foo.rb", "comment1")
-    a2 = make_annotation("bar.rb", "comment2")
-    out = Sight::AnnotationFormatter.format([a1, a2])
+    out = Sight::AnnotationFormatter.format([
+      make_annotation("foo.rb", "comment1"),
+      make_annotation("bar.rb", "comment2")
+    ])
 
     assert_includes out, "## File: foo.rb"
     assert_includes out, "## File: bar.rb"
@@ -51,11 +54,15 @@ class TestAnnotationFormatter < Minitest::Test
 
   private
 
-  def make_annotation(path, comment)
-    hunk = Sight::Hunk.new(context: "def example", lines: [
-      Sight::DiffLine.new(type: :del, content: "-old", lineno: nil),
-      Sight::DiffLine.new(type: :add, content: "+new", lineno: 1)
-    ])
-    Sight::Annotation.new(file_path: path, type: :hunk, hunk: hunk, comment: comment)
+  def make_annotation(file_path, comment, lines: [
+    Sight::DiffLine.new(type: :del, content: "-old", lineno: nil),
+    Sight::DiffLine.new(type: :add, content: "+new", lineno: 1)
+  ])
+    Sight::Annotation.new(
+      file_path:,
+      type: :hunk,
+      hunk: Sight::Hunk.new(context: "def example", lines:),
+      comment:
+    )
   end
 end
